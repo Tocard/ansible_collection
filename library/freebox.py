3cc98902ad1f9f94219b8419a3763603b4690ef7
Moz <contact@ether-source.fr>
2024-05-01 12:36:14 +0200

    remove comment

--- library/freebox.py
+++ library/freebox.py
@@ -374,7 +374,6 @@ def create_port_incoming(self, data: int):
 
 
 def run_module():
-    # define available arguments/parameters a user can pass to the module
     module_args = dict(
         app_id=dict(type='str', default='fr.freebox.integration'),
         app_name=dict(type='str', default='freebox_ansible)'),
@@ -389,39 +388,24 @@ def run_module():
         vault_mount_point=dict(type='str', required=True),
 
     )
-    # seed the result dict in the object
-    # we primarily care about changed and state
-    # changed is if this module effectively modified the target
-    # state will include any data that you want your module to pass back
-    # for consumption, for example, in a subsequent task
+
     result = dict(
         changed=False,
         original_message='',
         message=''
     )
 
-    # the AnsibleModule object will be our abstraction working with Ansible
-    # this includes instantiation, a couple of common attr would be the
-    # args/params passed to the execution, as well as if the module
-    # supports check mode
     module = AnsibleModule(
         argument_spec=module_args,
         supports_check_mode=True
     )
 
-    # if the user is working with this module in only check mode we do not
-    # want to make any changes to the environment, just return the current
-    # state with no modifications
     if module.check_mode:
         module.exit_json(**result)
 
-    # manipulate or modify the state as needed (this is going to be the
-    # part where your module will do what it needs to do)
     result['original_message'] = 'orginal_message'
     result['message'] = 'goodbye'
 
-    # use whatever logic you need to determine whether or not this module
-    # made any modifications to your target
     client = Freebox(app_id=module.params['app_id'], app_name=module.params['app_name'],
                      app_version=module.params['app_version'], device_name=module.params['device_name'],
                      freebox_url=module.params['freebox_url'],
@@ -435,17 +419,7 @@ def run_module():
     client.create_session()
     sample = client.get_static_lease()
     display.warning(sample)
-    # if module.params['new']:
-    #    result['changed'] = True
 
-    # during the execution of the module, if there is an exception or a
-    # conditional state that effectively causes a failure, run
-    # AnsibleModule.fail_json() to pass in the message and the result
-    # if module.params['name'] == 'fail me':
-    #    module.fail_json(msg='You requested this to fail', **result)
-
-    # in the event of a successful module execution, you will want to
-    # simple AnsibleModule.exit_json(), passing the key/value results
     module.exit_json(**result)
 
 
