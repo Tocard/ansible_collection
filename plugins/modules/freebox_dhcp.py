b82b5b87280e88df9572de0d0e36cba67d3e5a4b
Moz <contact@ether-source.fr>
2024-05-01 21:44:38 +0200

    add missing libs

--- plugins/modules/freebox_dhcp.py
+++ plugins/modules/freebox_dhcp.py
@@ -71,8 +71,9 @@ RETURN = r'''
 
 from ansible.module_utils.basic import AnsibleModule
 from ansible.utils.display import Display
-from plugins.module_utils.freebox_base import Freebox
 from ansible.module_utils.common import yaml
+
+from plugins.module_utils.freebox_base import Freebox
 
 import requests
 
