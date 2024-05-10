b6d231fec0dbc0b282d38b6785a3e4127f807ff3
Moz <moz@beer.com>
2024-05-11 01:40:58 +0200

    missing some config

---
+++ update.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+git fetch;git reset origin/master  --hard
+
+# Input semantic version
+
+# Activate virtual environment
+#source venv/Scripts/activate
+
+# Update version in galaxy.yml
+bump2version patch --verbose
+
+VERSION=$(cat marker)
+
+# Build and publish Ansible Galaxy Collection
+ansible-galaxy collection build . --force
+ansible-galaxy collection publish "tocard-utils-${VERSION}.tar.gz" --token 8105075849307ba61ab672979f38cc15d229dcc7
+
+# Create tag and push to Git
+git push origin --tags
+
+# Create a GitHub release and upload the artifact
+gh release create "${VERSION}" -n "Release ${VERSION}" -t "${VERSION}" "tocard-utils-${VERSION}.tar.gz"
+
+# Deactivate virtual environment (optional, depending on your needs)
+#deactivate
+
