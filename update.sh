ee91f7b2b5f237505981540e2ea85469bbaf452d
Moz <moz@beer.com>
2024-05-11 01:48:16 +0200

    add auto cleanup

--- update.sh
+++ update.sh
@@ -22,6 +22,7 @@ git push origin --tags
 # Create a GitHub release and upload the artifact
 gh release create "${VERSION}" -n "Release ${VERSION}" -t "${VERSION}" "tocard-utils-${VERSION}.tar.gz"
 
+rm *.gz -f
 # Deactivate virtual environment (optional, depending on your needs)
 #deactivate
 
