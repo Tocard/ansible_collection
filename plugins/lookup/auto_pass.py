1633633cfd19027935a861f0656590d8c3e3aa05
Moz <contact@ether-source.fr>
2024-05-19 23:01:08 +0200

    fix bad returns

--- plugins/lookup/auto_pass.py
+++ plugins/lookup/auto_pass.py
@@ -44,8 +44,10 @@ display = Display()
 
 
 class HVAC:
+    client = None
+
     def __init__(self):
-        self.client = self.init_hvac_client()
+        self.init_hvac_client()
 
     def init_hvac_client(self):
         try:
@@ -54,7 +56,7 @@ class HVAC:
             if not self.client.is_authenticated():
                 display.warning('client is not authenticated')
         except VaultError as e:
-            raise AnsibleError("Unable to init hvac client for address {}", os.getenv("hashi_vault_url"), e)
+            raise AnsibleError("Unable to init hvac client for address {}", os.getenv("VAULT_URL"), e)
 
     def read_path(self, mount_point: str, path: str) -> dict:
         try:
