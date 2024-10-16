5ea4ca2f113cb23775fe1c5b206a776d36ff2823
Moz <florian.goudot@gmail.com>
2024-10-16 21:10:19 +0200

    fix update & misc

--- update.sh
+++ update.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+TOKEN=$1
+
+if [ -z "${TOKEN}" ]; then
+  echo 'please pass token as parameter'
+  exit 1
+fi
+
 git fetch;git reset origin/master  --hard
 
 # Input semantic version
@@ -13,8 +20,8 @@ bump2version patch --verbose
 VERSION=$(cat marker)
 
 # Build and publish Ansible Galaxy Collection
-ansible-galaxy collection build . --force
-ansible-galaxy collection publish "tocard-utils-${VERSION}.tar.gz" --token 8105075849307ba61ab672979f38cc15d229dcc7
+ansible-galaxy collection build . --force --output-path  build/ --timeout 0 --verbose
+ansible-galaxy collection publish "tocard-utils-${VERSION}.tar.gz" --token "${TOKEN}"
 
 # Create tag and push to Git
 git push origin master --tags
