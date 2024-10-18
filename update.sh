ee11c9c96ce64dd9b45e3ecf4cc5d12319a6894f
John Doe <johndoe@example.com>
2024-10-18 22:37:50 +0200

    fix udate.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 TOKEN=$1
+BUILD_PATH="/build"
 
 if [ -z "${TOKEN}" ]; then
   echo 'please pass token as parameter'
@@ -20,14 +21,14 @@ bump2version patch --verbose
 VERSION=$(cat marker)
 
 # Build and publish Ansible Galaxy Collection
-ansible-galaxy collection build . --force --output-path  build/ --timeout 0 --verbose
-ansible-galaxy collection publish "tocard-utils-${VERSION}.tar.gz" --token "${TOKEN}"
+ansible-galaxy collection build . --force --output-path  ${BUILD_PATH} --timeout 0 --verbose
+ansible-galaxy collection publish "${BUILD_PATH}/tocard-utils-${VERSION}.tar.gz" --token "${TOKEN}"
 
 # Create tag and push to Git
 git push origin master --tags
 
 # Create a GitHub release and upload the artifact
-gh release create "${VERSION}" -n "Release ${VERSION}" -t "${VERSION}" "tocard-utils-${VERSION}.tar.gz"
+gh release create "${VERSION}" -n "Release ${VERSION}" -t "${VERSION}" "${BUILD_PATH}/tocard-utils-${VERSION}.tar.gz"
 
 rm *.gz -f
 # Deactivate virtual environment (optional, depending on your needs)
